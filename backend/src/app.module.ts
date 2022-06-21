8f73524e7be78a2dd58762fa3df41fe2f9df5cc9
Omar Diaaeldine <omardiaaelwakeel@gmail.com>
2022-06-21 18:33:55 +0200

    add typeorm module for db connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
 	imports: [
+		// configure .env file parser
 		ConfigModule.forRoot({
 			isGlobal: true
+		}),
+
+		// configure database configuration
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+
+			useFactory: async (configService: ConfigService) => {
+				return {
+					type: 'mysql',
+					host: configService.get<string>("DB_HOST"),
+					port: configService.get<number>("DB_PORT"),
+					username: configService.get<string>("DB_USERNAME"),
+					password: configService.get<string>("DB_PASSWORD"),
+					database: configService.get<string>("DB_NAME"),
+					entities: [__dirname + '/**/*.entity{.ts,.js}'],
+					synchronize: true,
+				}
+			},
+
+			inject: [ConfigService]
 		}),
 	],
 	controllers: [],
