97b6c6b79fba0f69de68579d36b8ef375449e01d
Omar Diaaeldine <omardiaaelwakeel@gmail.com>
2022-06-21 18:40:38 +0200

    add todo entity

---
+++ backend/src/modules/todos/todo.entity.ts
@@ -0,0 +1,34 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: 'todo', orderBy: { date: 'DESC' } })
+export class Todo extends BaseEntity {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Column({
+		nullable: false,
+		length: 100
+	})
+	name: string;
+
+	@Column({
+		nullable: false,
+		length: 255
+	})
+	description: string;
+
+	// @ManyToOne(type => User)
+	// @JoinColumn({ name: 'userId', referencedColumnName: 'id' })
+	// user: User;
+
+	// @ManyToOne(type => Status)
+	// @JoinColumn({ name: 'statusId', referencedColumnName: 'id' })
+	// status: Status;
+
+	@Column({
+		nullable: false,
+		default: Date.now(),
+		type: "bigint"
+	})
+	date: number;
+}
