08b0306703101afbfd82b8de9a2872844d59728f
Omar Diaaeldine <omardiaaelwakeel@gmail.com>
2022-06-21 19:42:41 +0200

    add base repository

---
+++ backend/src/modules/base/base-repository.ts
@@ -0,0 +1,104 @@
+import { BadRequestException, NotFoundException } from "@nestjs/common";
+import { BaseEntity, Repository } from "typeorm";
+
+export class BaseRepository<Entity extends BaseEntity> extends Repository<Entity>{
+	async getAll(): Promise<Entity[]> {
+		return this.find();
+	}
+
+	async getById(recordId: number): Promise<Entity> {
+		this.checkRecordIdOrFail(recordId);
+
+		const findConditions: any = {
+			where: {
+				id: recordId
+			}
+		}
+
+		const record = await this.findOne(findConditions);
+
+		if (!record) {
+			throw new NotFoundException(`${this.metadata.tableName} table has no record with id ${recordId}`)
+		}
+
+		return record;
+	}
+
+	async updateOne(recordId: number, updateDto: object): Promise<Entity> {
+		this.checkRecordIdOrFail(recordId);
+
+		const findConditions: any = {
+			where: {
+				id: recordId
+			}
+		}
+
+		const record = await this.findOne(findConditions);
+
+		if (!record) {
+			throw new NotFoundException(`${this.metadata.tableName} table has no record with id ${recordId}`)
+		}
+
+		// for every key in the relations to be loaded, check if it exists and update it
+		for (const key in updateDto) {
+			if (key !== "id" && this.metadata.hasColumnWithPropertyPath(key)) {
+				const field = updateDto[key];
+				record[key] = field
+			}
+		}
+
+		await record.save()
+
+		return record;
+	}
+
+	async addOne(createDto: object): Promise<Entity> {
+		const record = this.create();
+
+		// for every key in the relations to be loaded, check if it exists and add it
+		for (const key in createDto) {
+			const field = createDto[key];
+
+			if (this.metadata.hasColumnWithPropertyPath(key)) {
+				record[key] = field;
+			}
+		}
+
+		await record.save();
+
+		return record;
+	}
+
+	async deleteOne(recordId: number): Promise<{
+		id: number
+	}> {
+		this.checkRecordIdOrFail(recordId);
+
+		const findConditions: any = {
+			where: {
+				id: recordId
+			}
+		}
+
+		const record = await this.findOne(findConditions);
+
+		if (!record) {
+			throw new NotFoundException(`${this.metadata.tableName} table has no record with id ${recordId}`)
+		}
+
+		// extract id to return it to the frontend and update ui
+		const id = record["id"]
+
+		await record.remove();
+
+		return {
+			id
+		};
+	}
+
+	private checkRecordIdOrFail(recordId: number): void {
+		if (!recordId || typeof recordId === "undefined" || recordId === null) {
+			throw new BadRequestException('Record id sent is not valid')
+		}
+	}
+}
