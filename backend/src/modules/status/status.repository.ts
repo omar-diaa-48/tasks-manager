96424eec436d2fb243db86a4a0f8b8c29b58cbb6
Omar Diaaeldine <omardiaaelwakeel@gmail.com>
2022-06-23 16:58:45 +0200

    remove unecessary status repository

--- backend/src/modules/status/status.repository.ts
+++
@@ -1,7 +0,0 @@
-
-import { EntityRepository } from "typeorm";
-import { BaseRepository } from "../base/base-repository";
-import { Status } from "./status.entity";
-
-@EntityRepository(Status)
-export class StatusRepository extends BaseRepository<Status>{ }
