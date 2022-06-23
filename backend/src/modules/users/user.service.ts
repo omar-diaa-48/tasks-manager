57e3450b4c3b0d01f2c219c52e8f88d7614fc47c
Omar Diaaeldine <omardiaaelwakeel@gmail.com>
2022-06-23 16:48:42 +0200

    adjust user service signin flow

--- backend/src/modules/users/user.service.ts
+++ backend/src/modules/users/user.service.ts
@@ -1,6 +1,9 @@
 
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from "@nestjs/typeorm";
+import * as bcrypt from "bcrypt";
+import { JwtPayload } from "src/types/jwt-payload";
 import { BaseRepository } from "../base/base-repository";
 import { CredentialsDTO } from "./dto/credentials.dto";
 import { User } from "./user.entity";
@@ -9,14 +12,47 @@ import { User } from "./user.entity";
 export class UserService {
 	constructor(
 		@InjectRepository(User)
-		private repository: BaseRepository<User>
+		private repository: BaseRepository<User>,
+
+		private jwtService: JwtService
 	) { }
 
-	signup(signupDTO: CredentialsDTO): Promise<User> {
-		return this.repository.addOne(signupDTO);
+	async signup(signupDTO: CredentialsDTO): Promise<JwtPayload> {
+		const { username, password } = signupDTO;
+		let user: User;
+
+		user = await this.repository.findOneBy({ username })
+
+		if (user) {
+			throw new BadRequestException("Username already used")
+		}
+
+		user = new User();
+
+		// 1 for admin user while 2 for regular user
+		user.username = username;
+		user.password = this.hashPassword(password);
+
+		await user.save();
+
+		const payload: JwtPayload = {
+			id: user.id,
+			username: user.username
+		}
+
+		const accessToken = this.jwtService.sign(payload, { expiresIn: '21d' });
+
+		payload.accessToken = accessToken;
+
+		return payload;
 	}
 
-	signin(signupDTO: CredentialsDTO): Promise<User> {
-		return this.repository.addOne(signupDTO);
+	signin(signinDTO: CredentialsDTO): Promise<JwtPayload> {
+		console.log(signinDTO);
+		return this.repository.addOne(signinDTO);
+	}
+
+	hashPassword(password: string): string {
+		return bcrypt.hashSync(password, 10);
 	}
 }
