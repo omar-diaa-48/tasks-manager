5a4b0a27409b450575a4420b04f25983e13abb00
Omar Diaaeldine <omardiaaelwakeel@gmail.com>
2022-06-23 16:41:26 +0200

    add error handler in the store middleware

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,14 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, isAsyncThunkAction, isRejected } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
+import { showMessage } from './reducers/message';
 import user from "./reducers/user";
 
 const reducer = combineReducers({
 	user
 })
 
+const errorHandler = (api) => (next) => (action) => {
+	if (isAsyncThunkAction(action)) {
+		const { dispatch } = api;
+
+		//exclude get requests errors from being notified in the UI 
+		if (!action?.type?.includes('get')) {
+
+			if (isRejected(action)) {
+				let message = 'Failed to do the current operation'
+
+				if (action?.payload?.message) {
+					message = action?.payload?.message;
+				}
+
+
+				dispatch(showMessage(message))
+			}
+
+		}
+
+	}
+	return next(action)
+}
+
+const middlewares = [errorHandler];
+
 const store = configureStore({
 	reducer,
-	devTools: true // alow chrome developer tools
+	devTools: true, // alow chrome developer tools
+	middlewares
 })
 
 export default store;
