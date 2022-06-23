5b5e776b21ee283cd424efe1c8918c93affa3af0
Omar Diaaeldine <omardiaaelwakeel@gmail.com>
2022-06-23 16:22:31 +0200

    render links in Home page based on signed in user

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,28 +1,29 @@
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 export default function Header() {
 	const navigate = useNavigate()
 
-	// const user = useSelector(({ user }) => user.data);
+	const user = useSelector(({ user }) => user.data);
 
 	const renderLinks = () => {
-		// return user ? (
-		// 	<div className="flex flex-col justify-between items-center mt-8">
-		// 		<button onClick={() => navigate("/submit")} className="text-xl font-medium tracking-wider text-green-300 hover:text-green-500">
-		// 			Try it out
-		// 		</button>
-		// 	</div>
-		// ) : (
-		// 	<div className="flex flex-col justify-between items-center mt-8">
-		// 		<p className="text-xl font-medium tracking-wider text-green-300">Want to try?</p>
-		// 		<button onClick={() => navigate("/sign-in")} className="px-8 py-2 text-lg font-medium text-white transition-colors duration-300 transform bg-indigo-600 rounded hover:bg-indigo-500">
-		// 			Signin
-		// 		</button>
-		// 		<button onClick={() => navigate("/sign-up")} className="px-8 py-2 text-lg font-medium text-white transition-colors duration-300 transform">
-		// 			Or create an account
-		// 		</button>
-		// 	</div>
-		// )
+		return user ? (
+			<div className="flex flex-col justify-between items-center mt-8">
+				<button onClick={() => navigate("/submit")} className="text-xl font-medium tracking-wider text-green-300 hover:text-green-500">
+					Try it out
+				</button>
+			</div>
+		) : (
+			<div className="flex flex-col justify-between items-center mt-8">
+				<p className="text-xl font-medium tracking-wider text-green-300">Want to try?</p>
+				<button onClick={() => navigate("/sign-in")} className="px-8 py-2 text-lg font-medium text-white transition-colors duration-300 transform bg-indigo-600 rounded hover:bg-indigo-500">
+					Signin
+				</button>
+				<button onClick={() => navigate("/sign-up")} className="px-8 py-2 text-lg font-medium text-white transition-colors duration-300 transform">
+					Or create an account
+				</button>
+			</div>
+		)
 	}
 
 	return (
