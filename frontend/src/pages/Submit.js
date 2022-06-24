7e923411668a2e6b31bda7b91e8b568251c9d986
Omar Diaaeldine <omardiaaelwakeel@gmail.com>
2022-06-24 15:14:52 +0200

    add title and description to submit page

--- frontend/src/pages/Submit.js
+++ frontend/src/pages/Submit.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Input from "../components/shared/Input";
+import TextArea from "../components/shared/TextArea";
 
 export default function Submit() {
 	const [formValues, setFormValues] = useState({
@@ -6,6 +8,13 @@ export default function Submit() {
 		description: "",
 		statusId: 1
 	})
+
+	const handleChange = (e) => {
+		setFormValues((prevValue) => ({
+			...prevValue,
+			[e.target.name]: e.target.value
+		}))
+	}
 
 	const handleSubmit = () => { }
 
@@ -16,6 +25,10 @@ export default function Submit() {
 				<div className="flex items-center justify-center font-black m-3 mb-12">
 					<h1 className="tracking-wide text-3xl text-gray-900">Track your tasks</h1>
 				</div>
+
+				<Input type="text" handleChange={handleChange} value={formValues.title} name="title" title="Title" />
+
+				<TextArea handleChange={handleChange} value={formValues.description} name="description" title="Description" />
 
 				<button onClick={handleSubmit} className="px-4 py-1.5 rounded-md shadow-lg bg-gradient-to-r from-pink-600 to-red-600 font-medium text-gray-100 block transition duration-300">
 					<span>Submit</span>
