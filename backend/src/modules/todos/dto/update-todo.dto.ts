014d2d5a60973698dd55dc0fc93519d5cd8f7ef6
Omar Diaaeldine <omardiaaelwakeel@gmail.com>
2022-06-24 15:06:27 +0200

    adjust update todo dto

--- backend/src/modules/todos/dto/update-todo.dto.ts
+++ backend/src/modules/todos/dto/update-todo.dto.ts
@@ -1,17 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsDefined, IsNumber, IsString } from "class-validator";
+import { IsDefined, IsNumber } from "class-validator";
 
 export class UpdateTodoDTO {
-	@ApiProperty()
-	@IsDefined()
-	@IsString()
-	name: string;
-
-	@ApiProperty()
-	@IsDefined()
-	@IsString()
-	description: number;
-
 	@ApiProperty()
 	@IsDefined()
 	@IsNumber()
