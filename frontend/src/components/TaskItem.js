a2eb2fd943a01284cb61982d7f8540b3f1f2ae92
Omar Diaaeldine <omardiaaelwakeel@gmail.com>
2022-06-26 18:20:20 +0200

    adjust taskItem behavior on hover

--- frontend/src/components/TaskItem.js
+++ frontend/src/components/TaskItem.js
@@ -1,18 +1,36 @@
+import { useState } from 'react';
 import { Draggable } from 'react-beautiful-dnd';
+import { FaClock } from "react-icons/fa";
+import { useSelector } from 'react-redux';
 import { changeTimestampToDate } from '../utilities';
 
 export default function TaskItem({ id, index, title, owner, description, time, handleOpenTask }) {
+	const user = useSelector(({ user }) => user.data);
+
+	const [isOver, setIsOver] = useState(false)
+
+	const handleMouseOver = (isOver) => {
+		setIsOver(isOver)
+	}
+
 	return (
 		<Draggable key={id} draggableId={id} index={index}>
 			{(provided) => (
-				<div onClick={() => handleOpenTask(id, title)} className="bg-white p-2 rounded mt-1 cursor-pointer border-b border-grey hover:bg-grey-lighter"
+				<div
+					className="bg-white p-2 rounded mt-1 cursor-pointer border-b border-grey hover:bg-grey-lighter"
+					onMouseOver={() => handleMouseOver(true)}
+					onMouseLeave={() => handleMouseOver(false)}
+					onClick={() => handleOpenTask(id, title)}
 					ref={provided.innerRef}
 					{...provided.draggableProps}
 					{...provided.dragHandleProps}
 				>
-					<p className='mb-4 bg-slate-200'>{owner} @ {title}</p>
-					<p className='mb-2'>{changeTimestampToDate(time)}</p>
-					<p>{description}</p>
+					<p className={`mb-4 bg-slate-100 ${user.username !== owner && "text-stone-500"}`}>{owner} @ {title}</p>
+					<div className='flex flex-row items-center mb-2'>
+						<FaClock className='mr-2' />
+						<p>{changeTimestampToDate(time)}</p>
+					</div>
+					<p>{(user.username !== owner && !isOver) ? "" : description}</p>
 				</div>
 			)}
 		</Draggable >
