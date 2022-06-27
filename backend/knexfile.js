d08e6e4f9b34c1f9429346b4663a66176ec71d81
Omar Diaaeldine <omardiaaelwakeel@gmail.com>
2022-06-27 07:47:23 +0200

    use env variables in knexfile

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,13 +1,15 @@
+require("dotenv").config();
+
 module.exports = {
 
 	development: {
 		client: 'mysql',
 		connection: {
-			host: "127.0.0.1",
-			port: 3306,
-			user: "root",
-			password: "^8@tzCERA!c3ib",
-			database: "tasks-manager"
+			host: process.env.DB_HOST,
+			port: process.env.DB_PORT,
+			user: process.env.DB_USERNAME,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB_NAME
 		},
 		seeds: {
 			directory: './src/db/seeds'
